c8563ca7a3c5b318eae4ddef26ffe754ee3d6dd9
Alejandro Depetris <aledepetris@outlook.com>
2023-03-20 16:46:59 -0300

    Se desacoplo el codigo del sidebar de la app a su respectivo componente.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidebarComponent
