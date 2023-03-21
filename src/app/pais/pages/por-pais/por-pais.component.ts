076fcbe4f346a8a9a8b45cca0abce43ac9b92cce
Alejandro Depetris <aledepetris@outlook.com>
2023-03-21 16:56:28 -0300

    - Tabla de resultados Se agregó la logica necesaria para mostrar la lista de resultados

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,6 +12,8 @@ export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
+
   constructor( private paisService: PaisService) { }
 
   buscar() {
@@ -18,12 +21,11 @@ export class PorPaisComponent {
     console.log(this.termino);
     this.paisService.buscarPais( this.termino )
       .subscribe( paises => {
-        console.log(paises);
-
+        this.paises = paises;
 
       }, (err => {
         this.hayError = true;
-
+        this.paises = []
       }) )
 
   }
